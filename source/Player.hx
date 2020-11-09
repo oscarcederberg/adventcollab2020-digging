81b54899286559d792c4fcc5ad3af3658e1f9621
Knose <os7138ce-s@student.lu.se>
2020-11-09 15:03:34 +0100

    Prototype movement.

--- source/Player.hx
+++ source/Player.hx
@@ -2,7 +2,6 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxObject;
-import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.tile.FlxTilemap;
 import flixel.util.FlxColor;
@@ -17,11 +16,12 @@ enum Direction
 
 class Player extends FlxSprite
 {
-	static final MOVE_SPEED:Float = 16;
-	static final JUMP_SPEED:Float = 32;
-	static final GRAVITY:Float = 32;
+	static final MOVE_SPEED:Float = 32 * 4;
+	static final JUMP_SPEED:Float = 32 * 10;
+	static final GRAVITY:Float = 32 * 30;
 
 	var looking_at:Direction;
+	var jumping:Bool;
 	var tiles:FlxTilemap;
 
 	public function new(x:Float = 0, y:Float = 0, tiles:FlxTilemap)
@@ -30,15 +30,17 @@ class Player extends FlxSprite
 		this.tiles = tiles;
 
 		this.looking_at = Direction.Right;
+		this.jumping = false;
 		drag.x = MOVE_SPEED * 8;
 		acceleration.y = GRAVITY;
 		maxVelocity.x = MOVE_SPEED;
-		maxVelocity.y = 1.5 * JUMP_SPEED;
+		maxVelocity.y = JUMP_SPEED;
 		makeGraphic(32, 32, FlxColor.BLUE);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		FlxG.collide(this, tiles);
 		movement();
 		super.update(elapsed);
 	}
@@ -51,7 +53,7 @@ class Player extends FlxSprite
 		var _right:Bool = FlxG.keys.anyPressed([RIGHT, D]);
 
 		var _action:Bool = FlxG.keys.anyPressed([X, J]);
-		var _jump:Bool = FlxG.keys.anyJustPressed([C, K, SPACE]);
+		var _jump:Bool = FlxG.keys.anyPressed([C, K, SPACE]);
 
 		if (_up && _down)
 			_up = _down = false;
@@ -69,17 +71,23 @@ class Player extends FlxSprite
 
 		acceleration.x = 0;
 		if (_left)
-			acceleration.x = -drag.x;
+			acceleration.x = -maxVelocity.x * 4;
 		else if (_right)
-			acceleration.x = drag.x;
+			acceleration.x = maxVelocity.x * 4;
 
 		acceleration.y = GRAVITY;
 		if (this.isTouching(FlxObject.FLOOR))
 		{
 			if (_jump)
-				acceleration.y = -JUMP_SPEED;
+			{
+				jumping = true;
+				velocity.y = -maxVelocity.y;
+			}
 			else
+			{
+				jumping = false;
 				acceleration.y = 0;
+			}
 		}
 	}
 }
